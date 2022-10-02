0f84b780e0bb30ec4f00104abc85739eccf7dd44
gnncjegrgr <80614306+gnncjegrgr@users.noreply.github.com>
2022-10-02 20:28:40 +0900

    post model upload

---
+++ Back-end/models/post.js
@@ -0,0 +1,35 @@
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class Post extends Model {
+  static init(sequelize) {
+    return super.init({
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+	  category: {
+		type: DataTypes.TEXT,
+		allowNull: false,
+	  },
+	  private_mode: {
+	    type: DataTypes.BOOLEAN,
+	    allowNull: false,
+	  }
+    }, {
+      modelName: 'Post',
+      tableName: 'posts',
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+      sequelize,
+    });
+  }
+  static associate(db) {
+    db.Post.belongsTo(db.User);
+    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
+    db.Post.hasMany(db.Comment);
+    db.Post.hasMany(db.Image);
+	  db.Post.belongsToMany(db.User, { through: 'Scrap', as : 'Scrappers'});
+    db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' });
+  }
+};
