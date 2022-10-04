5141332524f13e7ec74510c353af48d83de5751d
gnncjegrgr <80614306+gnncjegrgr@users.noreply.github.com>
2022-10-04 21:48:08 +0900

    Update index.js

--- Back-end/passport/index.js
+++ Back-end/passport/index.js
@@ -10,7 +10,7 @@ module.exports = () => {
   passport.deserializeUser(async (id, done) => {
     try {
       const user = await User.findOne({ where: { id }});
-      done(null, user); // req.user
+      done(null, user);
     } catch (error) {
       console.error(error);
       done(error);
