f8bb79892c88a55c00635f02307d710701da92cc
gnncjegrgr <80614306+gnncjegrgr@users.noreply.github.com>
2022-10-04 21:44:19 +0900

    Update index.js

--- Back-end/models/index.js
+++ Back-end/models/index.js
@@ -4,6 +4,7 @@ const hashtag = require('./hashtag');
 const image = require('./image');
 const post = require('./post');
 const user = require('./user');
+const userid = require('./userid');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -16,6 +17,7 @@ db.Hashtag = hashtag;
 db.Image = image;
 db.Post = post;
 db.User = user;
+db.Userid = userid;
 
 Object.keys(db).forEach(modelName => {
   db[modelName].init(sequelize);
