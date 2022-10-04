de92b4369c44d1eeabdf50e0f1e951e890622dbb
gnncjegrgr <80614306+gnncjegrgr@users.noreply.github.com>
2022-10-04 21:47:23 +0900

    Create userid.js

---
+++ Back-end/models/userid.js
@@ -0,0 +1,23 @@
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class Userid extends Model {
+  static init(sequelize) {
+    return super.init({
+      my_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      }
+    }, {
+      modelName: 'Userid',
+      tableName: 'userids',
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+      sequelize,
+    });
+  }
+
+  static associate(db) {
+    db.Userid.belongsTo(db.Post);
+  }
+};
